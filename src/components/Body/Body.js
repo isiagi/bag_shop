9e9d3371c9490676b956b5a762e06c1535c704a7
isiagi <isiagigeofrey0@gmail.com>
2021-09-30 20:32:50 +0300

    adds Link and nav comp

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,38 +1,42 @@
- import React from 'react'
- import hev from '../images/bag.jpg'
- import './Body.css'
- 
- export default function Body() {
-     return (
-        <div>
-        <h3 className="h3__card">We have Got Your Style</h3>
-        <div className="card__container">
-            <div className="card__wrapper">
-                    <div className="card__image">
-                        <img src={hev} alt="logo"  width="350px"/>
-                    </div>
-                    <div className="card__info">
-                        <h4>{"heyy"}</h4>
-                        <h4>{"5$"}</h4>
-                    </div>
-                    <div className="card__button">
-                        <button type='submit'>Add To Cart</button>
-                    </div>   
-                </div>
-                <div className="card__wrapper">
-                    <div className="card__image">
-                        <img src={hev} alt="logo" width="350px"/>
-                    </div>
-                    <div className="card__info">
-                        <h4>{"heyy"}</h4>
-                        <h4>{"5$"}</h4>
-                    </div>
-                    <div className="card__button">
-                        <button type='submit'>Add To Cart</button>
-                    </div>   
-                </div>
+import React from 'react';
+import hev from '../images/bag.jpg';
+import './Body.css';
+import { Link } from 'react-router-dom';
+
+export default function Body() {
+  return (
+    <div>
+      <h3 className="h3__card">We have Got Your Style</h3>
+      <div className="card__container">
+        <div className="card__wrapper">
+          <div className="card__image">
+            <img src={hev} alt="logo" width="350px" />
+          </div>
+          <div className="card__info">
+            <h4>{'heyy'}</h4>
+            <h4>{'5$'}</h4>
+          </div>
+          <div className="card__button">
+            <button type="submit">
+              <Link to="/detail">Add To Cart</Link>
+            </button>
+          </div>
         </div>
+        <div className="card__wrapper">
+          <div className="card__image">
+            <img src={hev} alt="logo" width="350px" />
+          </div>
+          <div className="card__info">
+            <h4>{'heyy'}</h4>
+            <h4>{'5$'}</h4>
+          </div>
+          <div className="card__button">
+            <button type="submit">
+              <Link to="/detail">Add To Cart</Link>
+            </button>
+          </div>
         </div>
-     )
- }
- 
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Nav/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from './Nav'
+import Na from './Na'
+
+export default function index() {
+    return (
+        <div>
+            <Nav />
+            <Na />
+        </div>
+    )
+}
