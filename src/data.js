5535528a7076ed8159131b0d817bc05c5b83ef7b
Collins Beinomugisha <bex.collo13@gmail.com>
2021-10-05 10:57:30 +0300

    Adds Cart page

---
+++ src/data.js
@@ -0,0 +1,23 @@
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'Gucci Bag',
+        price: 1400,
+        image: '',
+      },
+      {
+        id: '2',
+        name: 'Laptop Bag',
+        price: 2400,
+        image: '',
+      },
+      {
+        id: '3',
+        name: 'Hand Bag',
+        price: 1000,
+        image: '',
+      },
+    ],
+  };
+  export default data;
