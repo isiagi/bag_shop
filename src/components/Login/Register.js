9f675c8a34fda96e521ce3128cb594661662ebb8
isiagi <isiagigeofrey0@gmail.com>
2021-10-05 22:18:47 +0300

    add server connection

--- src/components/Login/Register.js
+++ src/components/Login/Register.js
@@ -1,53 +1,131 @@
 import React from 'react';
-import './Register.css'
+import './Register.css';
 
-
-
-const { Component } = React
+const { Component } = React;
 
 class EntryPage extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      currentView: "signUp"
-    }
+      currentView: 'signUp',
+      input: {},
+    };
   }
 
   changeView = (view) => {
     this.setState({
-      currentView: view
-    })
-  }
+      currentView: view,
+    });
+  };
 
+  handleChange = (e) => {
+    let input = this.state.input;
+    input[e.target.name] = e.target.value;
+    this.setState({
+      input,
+    });
+  };
+
+  handleSignUp = (e) => {
+    e.preventDefault();
+    fetch('http://localhost:9000/api/v1/user/register', {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      body: JSON.stringify({
+        email: this.state.input.email,
+        password: this.state.input.password,
+        username: this.state.input.username,
+      }),
+    })
+      .then((doc) => {
+        if (doc.ok) {
+          return doc.json();
+        } else {
+          throw new Error('something has happened');
+        }
+      }).then((res) => {
+        localStorage.setItem("token", res.token);
+        window.location.href = "/";
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+  
+  handleLogin = (e) => {
+    e.preventDefault();
+    fetch('http://localhost:9000/api/v1/user/login', {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      body: JSON.stringify({
+        email: this.state.input.email,
+        password: this.state.input.password,
+      }),
+    })
+      .then((doc) => {
+        if (doc.ok) {
+          return doc.json();
+        } else {
+          throw new Error('something has happened');
+        }
+      }).then((res) => {
+        localStorage.setItem("token", res.token);
+        window.location.href = "/";
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
   currentView = () => {
-    switch(this.state.currentView) {
-      case "signUp":
+    switch (this.state.currentView) {
+      case 'signUp':
         return (
           <form>
+            {this.state.input.email}
             <h2>Sign Up!</h2>
             <fieldset>
               <legend>Create Account</legend>
               <ul>
                 <li>
                   <label for="username">Username:</label>
-                  <input type="text" id="username" required/>
+                  <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    onChange={this.handleChange}
+                    required
+                  />
                 </li>
                 <li>
                   <label for="email">Email:</label>
-                  <input type="email" id="email" required/>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    onChange={this.handleChange}
+                    required
+                  />
                 </li>
                 <li>
                   <label for="password">Password:</label>
-                  <input type="password" id="password" required/>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    onChange={this.handleChange}
+                    required
+                  />
                 </li>
               </ul>
             </fieldset>
-            <button>Submit</button>
-            <button type="button" onClick={ () => this.changeView("logIn")}>Have an Account?</button>
+            <button onClick={this.handleSignUp}>Submit</button>
+            <button type="button" onClick={() => this.changeView('logIn')}>
+              Have an Account?
+            </button>
           </form>
-        )
-        break
-      case "logIn":
+        );
+        break;
+      case 'logIn':
         return (
           <form>
             <h2>Welcome Back!</h2>
@@ -56,58 +134,76 @@ class EntryPage extends Component {
               <ul>
                 <li>
                   <label for="username">Username:</label>
-                  <input type="text" id="username" required/>
+                  <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    onChange={this.handleChange}
+                    required
+                  />
                 </li>
                 <li>
                   <label for="password">Password:</label>
-                  <input type="password" id="password" required/>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    onChange={this.handleChange}
+                    required
+                  />
                 </li>
                 <li>
-                  <i/>
-                  <a onClick={ () => this.changeView("PWReset")} href="#">Forgot Password?</a>
+                  <i />
+                  <a onClick={() => this.changeView('PWReset')} href="#">
+                    Forgot Password?
+                  </a>
                 </li>
               </ul>
             </fieldset>
-            <button>Login</button>
-            <button type="button" onClick={ () => this.changeView("signUp")}>Create an Account</button>
+            <button onClick={this.handleLogin}>Login</button>
+            <button type="button" onClick={() => this.changeView('signUp')}>
+              Create an Account
+            </button>
           </form>
-        )
-        break
-      case "PWReset":
+        );
+        break;
+      case 'PWReset':
         return (
           <form>
-          <h2>Reset Password</h2>
-          <fieldset>
-            <legend>Password Reset</legend>
-            <ul>
-              <li>
-                <em>A reset link will be sent to your inbox!</em>
-              </li>
-              <li>
-                <label for="email">Email:</label>
-                <input type="email" id="email" required/>
-              </li>
-            </ul>
-          </fieldset>
-          <button>Send Reset Link</button>
-          <button type="button" onClick={ () => this.changeView("logIn")}>Go Back</button>
-        </form>
-        )
+            <h2>Reset Password</h2>
+            <fieldset>
+              <legend>Password Reset</legend>
+              <ul>
+                <li>
+                  <em>A reset link will be sent to your inbox!</em>
+                </li>
+                <li>
+                  <label for="email">Email:</label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    onChange={this.handleChange}
+                    required
+                  />
+                </li>
+              </ul>
+            </fieldset>
+            <button onClick={this.handleReset}>Send Reset Link</button>
+            <button type="button" onClick={() => this.changeView('logIn')}>
+              Go Back
+            </button>
+          </form>
+        );
       default:
-        break
+        break;
     }
-  }
-
-
+  };
   render() {
     return (
-      <section id="entry-page">
-        {this.currentView()}
-      </section>
-    )
+    <section id="entry-page">{this.currentView()}</section>
+    );
   }
 }
 
 export default EntryPage;
-
-// ReactDOM.render(<EntryPage/>, document.getElementById("app"))
