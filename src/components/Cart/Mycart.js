0d7dd242847f413a6438f415ae239edbb45f3f61
isiagi <isiagigeofrey0@gmail.com>
2021-10-19 17:02:56 +0300

    adds context api

--- src/components/Cart/Mycart.js
+++ src/components/Cart/Mycart.js
@@ -1,39 +1,16 @@
-import React from 'react';
-import Header from './Header';
-import Main from './Main';
-import Basket from './Basket';
-import data from '../../data';
-import { useState } from 'react';
-import './Kart.css';
+import React, { useContext, useState } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Basket from "./Basket";
+import data from "../../data";
+import { AppContext } from "../Context/Context";
 
+import "./Kart.css";
 
 function Cart() {
   const { products } = data;
-  const [cartItems, setCartItems] = useState([]);
-  const onAdd = (product) => {
-    const exist = cartItems.find((x) => x.id === product.id);
-    if (exist) {
-      setCartItems(
-        cartItems.map((x) =>
-          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
-        )
-      );
-    } else {
-      setCartItems([...cartItems, { ...product, qty: 1 }]);
-    }
-  };
-  const onRemove = (product) => {
-    const exist = cartItems.find((x) => x.id === product.id);
-    if (exist.qty === 1) {
-      setCartItems(cartItems.filter((x) => x.id !== product.id));
-    } else {
-      setCartItems(
-        cartItems.map((x) =>
-          x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
-        )
-      );
-    }
-  };
+  const [cartItems, onAdd, onRemove] = useContext(AppContext);
+  
   return (
     <div className="App">
       <Header countCartItems={cartItems.length}></Header>
--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -5,20 +5,23 @@ import Mycart from "../components/Cart/Mycart";
 import Detail from "../components/Detail/Detail";
 import Reset from "../components/Reset/ResetPasswordScreen";
 import ScrollToTop from "react-router-scroll-top";
+import { Context } from "../components/Context/Context";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default function index() {
   return (
     <Router>
-      <Switch>
-        <ScrollToTop>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/Register" component={Register} />
-          <Route path="/Mycart" component={Mycart} />
-          <Route path="/detail" component={Detail} />
-          <Route path="/passwordreset/:id" exact component={Reset} />
-        </ScrollToTop>
-      </Switch>
+      <Context>
+        <Switch>
+          <ScrollToTop>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/Register" component={Register} />
+            <Route path="/Mycart" component={Mycart} />
+            <Route path="/detail" component={Detail} />
+            <Route path="/passwordreset/:id" exact component={Reset} />
+          </ScrollToTop>
+        </Switch>
+      </Context>
     </Router>
   );
 }
