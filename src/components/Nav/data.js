837713f29c93987d1db1db69590d9b616190451a
isiagi <isiagigeofrey0@gmail.com>
2022-01-17 13:26:41 +0300

    add drop down

---
+++ src/components/Nav/data.js
@@ -0,0 +1,22 @@
+export const data = [
+    {
+        id: 1,
+        title: 'Bags',
+    },
+    {
+        id: 1,
+        title: 'Shoes',
+    },
+    {
+        id: 1,
+        title: 'Cloths',
+    },
+    {
+        id: 1,
+        title: 'Accessory',
+    },
+    {
+        id: 1,
+        title: 'Sign Up',
+    }
+]
---
+++ src/components/Nav/g.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import DropDown from "./DropDown";
+import { data } from "./data";
+import "./g.css";
+const g = () => {
+  const [drop, setDrop] = React.useState(false);
+  const [dro, setDro] = React.useState(false);
+  return (
+    <div className="g__wrapper">
+      <ul className="lo">
+        {data.map((item) => {
+            if (item.title === "Bags") {
+            return (
+              <li
+                key={item.id}
+                onMouseEnter={() => setDrop(true)}
+                onMouseLeave={() => setDrop(false)}
+                className="nav__i"
+              >
+                <Link>
+                  {item.title}
+                  {drop && <DropDown />}
+                </Link>
+              </li>
+            );
+          }
+
+          if (item.title === "Shoes") {
+            return (
+              <li
+                key={item.id}
+                onMouseEnter={() => setDro(true)}
+                onMouseLeave={() => setDro(false)}
+              >
+                <Link>
+                  {item.title}
+                  {dro && <DropDown />}
+                </Link>
+              </li>
+            );
+          }
+          
+          return (
+            <li key={item.id}>
+              <Link>{item.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default g;
